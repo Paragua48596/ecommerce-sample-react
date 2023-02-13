47ca74ef2ec8e7b0d3cdd85e38889b60c31b93e0
Samuel Gonzalez <sigpay@outlook.com>
2023-02-13 16:01:11 -0300

    Importamos imagenes Creamos estructura completa del menu.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,29 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+
+//Imagenes
+import user_icon from '../public/images/user.png'
+import cart_icon from '../public/images/cart.png'
 
 class Menu extends Component {
-    render(){
+    render() {
         return (
-            <h1>Menu</h1>
+            <nav className='navbar'>
+                <div className='logo'>
+                    <h1 className='logo__name'>Logo</h1>
+                </div>
+
+                <div className='search'>
+                    <form action=''>
+                        <input type="text" placeholder="Search..." name="search" />
+                        <button type="submit"><i class="fa fa-search"></i></button>
+                    </form>
+                </div>
+
+                <div className='icons'>
+                    <img src={cart_icon} className='icons__img' alt='carrito de compras'/>
+                    <img src={user_icon} className='icons__img' alt='usuario'/>
+                </div>
+            </nav>
         )
     }
 }
