6ff251dd45d0be9c0ea3c08b6df5e4373b35dd6e
Samuel Gonzalez <sigpay@outlook.com>
2023-02-21 18:57:07 -0300

    Importamos y llamamos al elemento del Menu.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 //Componentes
+import Menu from '../components/Menu';
 import Bannner from '../components/Banner';
 import Categories from '../components/Categories';
 
@@ -8,6 +9,7 @@ class Home extends Component {
     render() {
         return (
             <React.Fragment>
+                <Menu />
                 <Bannner />
                 <main>
                     <Categories />
